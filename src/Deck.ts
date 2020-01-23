ca43de14f4b2565d48eed5efbb17d64253c76903
KoutaKawase <suhrkawase@gmail.com>
2020-01-23 17:43:36 +0900

    🚧 デッキ初期化処理

--- src/Deck.ts
+++ src/Deck.ts
@@ -1,1 +1,19 @@
-export class Deck {}
+import { Card } from "./Card";
+
+export class Deck {
+  private _cardList: Card[];
+
+  constructor() {
+    this._cardList = [];
+  }
+  //単純にシャッフルせずデッキ内のカードを作成
+  public init(): void {
+    for (let i = 0; i < 15; i++) {
+      this._cardList.push(new Card());
+    }
+  }
+
+  get cardList(): Card[] {
+    return this._cardList;
+  }
+}
